6442aaaac20e57ad7b90e606c07ecd3728d04a8a
Henning <fakestreet@nurfuerspam.de>
2019-04-29 14:31:42 +0200

    add first chapter version 0.1

--- R/DataIn.R
+++ R/DataIn.R
@@ -3,19 +3,17 @@
 
 # The data in this project comes from mortality.org
 
-convert_data() {
+ConvertData = function() {
   # read the data
   deaths1990 = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/1 total population (1990-2017)/cleaned/1-year-germany-deaths-clean", header = TRUE)
-
   births1990 = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/1 total population (1990-2017)/cleaned/1-year-germany-births-cleaned", header = TRUE)
-
   deathrates1990 = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/1 total population (1990-2017)/cleaned/1-year-germany-death-rates-cleaned", header = TRUE)
+  popsize1990 = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/1 total population (1990-2017)/cleaned/1-year-germany-popsize-cleaned", header = TRUE)
 
   # save the data
   devtools::use_data(deaths1990, overwrite = T)
-
   devtools::use_data(births1990, overwrite = T)
-
   devtools::use_data(deathrates1990, overwrite = T)
+  devtools::use_data(popsize1990, overwrite = T)
 
 }
--- R/DeskriptiveScripts.R
+++ R/DeskriptiveScripts.R
@@ -1,9 +1,23 @@
 # The functions in this file generate some visual summary of the data used in this
 # package.
 
-makeHistogram() {
+MakeBarplot = function() {
 # this function is supposed to generate a histogram, that ...
 
+  graphicspath = "../../1 Doku/graphics/einleitung_barplot.pdf"
 
+  years = 1990:2017
+
+  # Sterblichkeit in den Jahren 1990 und 2017
+  deaths.1990 = subset(deaths1990, deaths1990$Year == 1990)
+  deaths.2017 = subset(deaths1990, deaths1990$Year == 2017)
+
+  pdf(graphicspath, width = 10, height = 8)
+  par(mfrow=c(1,2))
+  barplot(deaths.1990$Total, horiz = TRUE, xlab = "Anzahl Tode 1990", ylab = "Alter",
+          names.arg = 0:110)
+  barplot(deaths.2017$Total, horiz = TRUE, xlab = "Anzahl Tode 2017", ylab = "Alter",
+          names.arg = 0:110)
+  dev.off()
 
 }
