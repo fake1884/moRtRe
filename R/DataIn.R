0d116abf380b910fc8ed3043264dfd602df1d3ff
Henning <fakestreet@nurfuerspam.de>
2019-05-05 20:59:21 +0200

    Update Doku

--- R/DataIn.R
+++ R/DataIn.R
@@ -9,11 +9,16 @@ ConvertData = function() {
   births1990 = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/1 total population (1990-2017)/cleaned/1-year-germany-births-cleaned", header = TRUE)
   deathrates1990 = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/1 total population (1990-2017)/cleaned/1-year-germany-death-rates-cleaned", header = TRUE)
   popsize1990 = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/1 total population (1990-2017)/cleaned/1-year-germany-popsize-cleaned", header = TRUE)
+  deathrates1879west = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/2 west germany (1876 - 1987)/cleaned/death rates 1876 - 1987 west cleaned", header = TRUE)
+  deathrates1879east = read.table("/home/henning/Desktop/Masterarbeit Sterbetrends/2 Daten/3 east germany (1879 - 1987 )/cleaned/death rates 1879 - 1987 east cleaned", header = TRUE)
 
   # save the data
   devtools::use_data(deaths1990, overwrite = T)
   devtools::use_data(births1990, overwrite = T)
   devtools::use_data(deathrates1990, overwrite = T)
   devtools::use_data(popsize1990, overwrite = T)
+  devtools::use_data(deathrates1879west, overwrite = T)
+  devtools::use_data(deathrates1879east, overwrite = T)
 
 }
+
